f7e04db8b9635f0adf67d9f7488ae64f291a564c
IamBusy <1342247033@qq.com>
2018-06-07 11:52:29 +0800

    Add constants

---
+++ constants/topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: william
+@contact: 1342247033@qq.com
+@site: http://www.xiaolewei.com
+@file: topics.py
+@time: 03/05/2018 15:34
+"""
+
+
+AD_PROCESSED_TOPIC = 'ad.statistic'                     # 加工过的统计数据
+AD_ORIGIN_STATISTIC_TOPIC = 'ad.original.statistic'     # 由Client上报的原始数据
+AGENCY_COMMAND_TOPIC = 'agency.command'                 # 下发command给agency
+AGENCY_COMMAND_REPORTER_TOPIC = 'agency.command.reporter'  # agency回传命令执行结果
+
+AD_CAMPAIGN_INFO_TOPIC = 'ad.campaign.info'             # 投放计划信息
